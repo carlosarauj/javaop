fe5c4e3900c2e3e893482d9463575a061a6b0d6d
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-01-22 18:14:15 -0300

    Add files via upload

---
+++ operadores01/src/operadores01/Principal.java
@@ -0,0 +1,12 @@
+package operadores01;
+
+public class Principal {
+
+	public static void main (String[] args) {
+		System.out.println(9 + 2);
+		System.out.println(9 - 2);
+		System.out.println(9 * 2);
+		System.out.println(9 / 2);
+	}
+}
+
